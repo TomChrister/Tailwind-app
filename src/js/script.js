60936d3b1d7e34ad468bf4266d85e823f4160a62
TOMCHRISTER\tomch <tomsch01266@stud.noroff.no>
2024-11-06 22:54:44 +0100

    testing deployment

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,12 @@
 document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const isValid = true;
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-    if (!isValid) {
-        window.location.href = 'feed.html'
+    if (username.length >= 3 && password.length >= 8) {
+        window.location.href = 'feed.html';
+    } else {
+        alert('Username must be min 3 characters and password must be min 8 characters');
     }
 });
